354c0b42c58afaa7224f8c3193b567775936db83
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-01-31 02:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // your code here
+
+
 function buttonClick() {
 	let name=document.getElementById("name").value
-	let year=document.getElementById("year").value
+    let year=document.getElementById("year").value
 	let url=document.getElementById("url")
-	/*if (name!="" && year!="") {
-		url=("https://localhost:8080/name")
-	}*/
 	
-	url.innerText="masklcxnjasnkasnamasxa"
-	console.log(url.innerText)
+	if (name!="" && year!="") {
+		url.innerText="https://localhost:8080/?name="+`${name}&year=${year}`
+	}
+    else if(name!=""){
+		url.innerText="https://localhost:8080/?name="+`${name}`
+	}
+    else if(year!=""){
+		url.innerText="https://localhost:8080/?year="+`${year}`
+	}
 }
