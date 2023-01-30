b0a5c8640bad1bd9d44da0ac326146a6931528ad
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-01-30 21:50:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // your code here
-let name=document.getElementById("name")
-let yesr=document.getElementById("year")
-
 function buttonClick() {
+	let name=document.getElementById("name").value
+	let year=document.getElementById("year").value
+	let url=document.getElementById("url")
+	/*if (name!="" && year!="") {
+		url=("https://localhost:8080/name")
+	}*/
 	
+	url.innerText="masklcxnjasnkasnamasxa"
+	console.log(url.innerText)
 }
